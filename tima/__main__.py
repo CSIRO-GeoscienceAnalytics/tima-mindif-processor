d72e0a0f3798154116d917e2d365b859e1faa162
Sam Bradley <sam.bradley@csiro.au>
2020-01-16 22:16:27 +0800

    wip fixes

---
+++ tima/__main__.py
@@ -0,0 +1,23 @@
+import argparse
+from . tima_mindif_processor import tima_mindif_processor as tima14
+from . tima_mindif_processor_16 import tima_mindif_processor as tima16
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process TIMA data')
+    parser.add_argument('project_path', type=str, help='Path to the TIMA project')
+    parser.add_argument('mindif_root', type=str, help='Path to the MinDif root')
+    parser.add_argument('--output', '-o', dest="output", default="./",
+                        type=str, help='Path to the desired output folder')
+    parser.add_argument('--tima_version', '-t', dest="tima_version", choices=[
+                        "1.4", "1.6"], default="1.6", type=str, help='Version of TIMA default 1.6, ')
+    args = parser.parse_args()
+
+    if(args.tima_version == "1.6"):
+        tima16(args.project_path, args.mindif_root, args.output)
+    else:
+        tima14(args.project_path, args.mindif_root, args.output)
+
+
+if __name__ == "__main__":
+    main()
